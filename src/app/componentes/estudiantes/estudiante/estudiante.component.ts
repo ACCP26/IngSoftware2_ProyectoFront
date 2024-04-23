4185352a22b9663ce385cb251a4d4ab7f12f1eb5
ACCP <acorchuelop@ucentral.edu.co>
2024-04-23 16:05:14 -0500

    Componente y servicio de registro de estudiantes

--- src/app/componentes/estudiantes/estudiante/estudiante.component.ts
+++ src/app/componentes/estudiantes/estudiante/estudiante.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit} from '@angular/core';
+import {ActivatedRoute, Router, RouterModule} from "@angular/router"
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {EstudiantesService} from "../../servicios/estudiante.service";
+import {Estudiante} from "../estudiante";
+import {Observable} from "rxjs";
+import {response} from "express";
+
 
 @Component({
   selector: 'app-estudiante',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,ReactiveFormsModule],
   templateUrl: './estudiante.component.html',
   styleUrl: './estudiante.component.css'
 })
-export class EstudianteComponent {
+export default class EstudianteComponent implements OnInit{
 
+  private f_builder = inject(FormBuilder);
+  private estudianteServicio = inject(EstudiantesService);
+  private router = inject(Router);
+  private router = inject(ActivatedRoute);
+  form? : FormGroup;
+  estudiante? : Estudiante;
+  errors?: string[]
+
+
+  registrar(){
+
+    if(this.form?.invalid){
+        return;
+    }
+    let request: Observable<Estudiante>;
+    const estudiante = this.form!.value;
+
+    request = this.estudianteServicio.registrar(estudiante);
 }
--- src/app/servicios/estudiantes/estudiante.service.ts
+++ src/app/servicios/estudiantes/estudiante.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import {HttpClient,HttpHeaders} from "@angular/common/http";
+import {Estudiante} from "../componentes/estudiantes/estudiante";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstudianteService {
 
-  constructor() { }
+  urlRest = 'http://localhost:8863/api/estudiantes';
+
+  private httpClient = inject(httpClient)
+
+  httpOptions = {headers : new HttpHeaders({'Content-Type' : 'application/json'})};
+
+  public registrar(estudiante: Estudiante){
+    return this.httpClient.post<Estudiante>(this.urlRest,estudiante,this.httpOptions);
+  }
 }
